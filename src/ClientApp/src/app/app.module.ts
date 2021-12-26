d3deb5c4dbe0bc1b41e5405c188b0ac869a58408
Artem Hontar iMac <artsiom.hontar@ihsmarkit.com>
2021-12-26 21:46:31 +0400

    feat: black design

--- src/ClientApp/src/app/app.module.ts
+++ src/ClientApp/src/app/app.module.ts
@@ -9,16 +9,21 @@ import { FeedService } from './feed.service';
 import { CitationComponent } from './citation/citation.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { HomeComponent } from './home/home.component';;
-import { ArticleComponent } from './article/article.component'
-
+import { ArticleComponent } from './article/article.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component'
+;
+import { FooterComponent } from './footer/footer.component'
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'sources', component: ArticlesListComponent },
+    { path: 'recommendations', component: RecommendationsComponent },
     { path: 'articles', component: ArticleComponent }
 ];
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
-    declarations: [AppComponent, NotFoundComponent, CitationComponent, ArticlesListComponent, HomeComponent, ArticleComponent],
+    declarations: [AppComponent, NotFoundComponent, CitationComponent, ArticlesListComponent, HomeComponent, ArticleComponent, NavbarComponent, RecommendationsComponent, FooterComponent],
     providers: [FeedService], 
     bootstrap: [AppComponent]
 })
